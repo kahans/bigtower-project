6ae77ff7dc7bd47de5971a5ed387b174039c2a8a
Administrator <Administrator@smart20111>
2017-02-15 17:25:43 +0900

    병원 진료 사이트 미완성 및 파일 폴더 이름 변경 / 이동

--- bigtower/src/main/java/com/team4/project/HomeController.java
+++ bigtower/src/main/java/com/team4/project/HomeController.java
@@ -24,16 +24,19 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		return "government-index";
+	}
+	@RequestMapping(value = "/ho", method = RequestMethod.GET)
+	public String ho(Model model) {
+	
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "go-index";
+		return "hospital-index";
+	}
+	@RequestMapping(value = "/icons", method = RequestMethod.GET)
+	public String icons(Model model) {
+				
+		return "icons";
 	}
 	
 }
