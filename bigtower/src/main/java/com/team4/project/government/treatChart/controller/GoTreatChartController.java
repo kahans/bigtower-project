7339c729d5d1873ae6a307560fddeb3ef6ea50c7
Administrator <Administrator@smart20112>
2017-02-17 15:18:33 +0900

    GoTreatChartController 메소드 삭제

--- bigtower/src/main/java/com/team4/project/government/treatChart/controller/GoTreatChartController.java
+++ bigtower/src/main/java/com/team4/project/government/treatChart/controller/GoTreatChartController.java
@@ -16,14 +16,7 @@ public class GoTreatChartController {
 	
 	@Autowired
 	private GoTreatChartService goTCS;
-	
 
-	@RequestMapping(value="/government/treatView", method=RequestMethod.GET)
-	public String goSelectTreat(Model model){
-		logger.debug(goTCS.goSelectTreat().toString());
-		model.addAttribute("Treat",goTCS.goSelectTreat());
-		return "/government_si/goSelectTreat";
-	}
 
 	
 }
