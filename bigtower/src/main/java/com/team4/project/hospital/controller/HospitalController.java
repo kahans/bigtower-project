6a2b90a467a6cc2cca87e12cbff01a2eb4af8fe9
loverman85 <loverman85@hanmail.net>
2017-02-21 16:08:56 +0900

    병원로그인후 세션등록까지

--- bigtower/src/main/java/com/team4/project/hospital/controller/HospitalController.java
+++ bigtower/src/main/java/com/team4/project/hospital/controller/HospitalController.java
@@ -1,5 +1,59 @@
 package com.team4.project.hospital.controller;
 
-public class HospitalController {
+import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.team4.project.hospital.dto.HoStaff;
+import com.team4.project.hospital.receiveReservation.controller.HoReceiveReservationController;
+
+@Controller
+public class HospitalController {
+	private static final Logger logger = LoggerFactory.getLogger(HospitalController.class);
+
+	@Autowired
+	private HospitalService hoService;
+	
+	//인덱스 화면 보기
+	@RequestMapping(value="/hospital/", method=RequestMethod.GET)
+	public String index(Model model, HttpSession session){
+		if( session.getAttribute("hospitalCode")!=null){
+			model.addAttribute("hospitalCode", session.getAttribute("hospitalCode"));
+			model.addAttribute("staffLevelCode", session.getAttribute("staffLevelCode"));
+			model.addAttribute("hoStaffName", session.getAttribute("hoStaffName"));
+		}
+		return "/hospital_YJ/index";
+	}
+	
+	//로그인페이지 보기
+	@RequestMapping(value="/hospital/login", method=RequestMethod.GET)
+	public String login(){
+		return "/hospital_YJ/login";
+	}
+	
+	//로그인 처리
+	@RequestMapping(value="/hospital/login", method=RequestMethod.POST)
+	public String login(String hoStaffId, String hoStaffPw, HttpSession session){
+		logger.debug("hoStaffId:"+hoStaffId);
+		logger.debug("hoStaffPw:"+hoStaffPw);
+		HoStaff staff = hoService.loginCheck(hoStaffId, hoStaffPw);
+		if (staff!=null){
+			session.setAttribute("hospitalCode", staff.getHoHospitalCode());
+			session.setAttribute("staffLevelCode", staff.getStaffLevelCode());
+			session.setAttribute("hoStaffName", staff.getHoStaffName());
+			logger.debug("session hospitalCode:"+session.getAttribute("hospitalCode"));
+			logger.debug("session staffLevelCode:"+session.getAttribute("staffLevelCode"));
+			logger.debug("session hoStaffName:"+session.getAttribute("hoStaffName"));
+		}
+/*		ho_hospital_code,
+		staff_level_code,
+		ho_staff_name*/
+		return "redirect:/hospital/login";
+	}
 }
--- bigtower/src/main/java/com/team4/project/hospital/controller/HospitalService.java
+++ bigtower/src/main/java/com/team4/project/hospital/controller/HospitalService.java
@@ -1,5 +1,23 @@
 package com.team4.project.hospital.controller;
 
-public class HospitalService {
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.team4.project.hospital.dto.HoStaff;
+
+@Service
+public class HospitalService {
+	@Autowired
+	private HospitalDao hoDao;
+	
+	public HoStaff loginCheck(String hoStaffId, String hoStaffPw){
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("hoStaffId", hoStaffId);
+		map.put("hoStaffPw", hoStaffPw);
+		return hoDao.loginCheck(map);
+	}
+	
 }
