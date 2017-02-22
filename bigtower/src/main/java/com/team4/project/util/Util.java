6b042249c03daf0b3abc7eb3feafe4568d7f73e3
ryun215 <ryun215@naver.com>
2017-02-22 15:04:16 +0900

    util.Util에 공백에 따라 날짜 만들어주는 메소드 만듬

--- bigtower/src/main/java/com/team4/project/util/Util.java
+++ bigtower/src/main/java/com/team4/project/util/Util.java
@@ -11,20 +11,6 @@ import org.slf4j.LoggerFactory;
 public class Util {
 	private static final Logger logger = LoggerFactory.getLogger(Util.class);
 
-	
-	public static void main(String[] args){
-		String a = "2015-02-13";
-		String b = "";
-		
-		Map<String , Object> map= createDate(a , b);
-		System.out.println("firstDate 확인 : "+map.get("firstDate")+" secondDate : "+map.get("secondDate"));
-		
-		
-		
-	}
-	
-	
-	
 	public static Map<String, Object> createDate(String firstDate, String secondDate){
 		System.out.println("입력된 firstDate 확인 : "+firstDate);
 		System.out.println("입력된 secondDate 확인 : "+secondDate);
@@ -35,21 +21,35 @@ public class Util {
 		String currentdate = transFormat.format(date);
 		System.out.println("currentDate 확인 : "+currentdate);
 		if(firstDate.equals("")&secondDate.equals("")){
-			map.put("firstDate", "1900-01-01");
-			map.put("secondDate", currentdate);
-			logger.debug("firstDate 확인 : "+map.get("firstDate"));
-			logger.debug("secondDate 들어갔는지 확인 : "+map.get("secondDate"));
 			logger.debug("둘다 공백일때");
-		}else if(firstDate.equals("")){
 			map.put("firstDate", "1900-01-01");
-			logger.debug("firstDate 확인 : "+map.get("firstDate"));
-			logger.debug("앞이 공백일때");
-		}else{
 			map.put("secondDate", currentdate);
-			logger.debug("뒤가 공백일때");
+			logger.debug("firstDate 확인 : "+map.get("firstDate"));
 			logger.debug("secondDate 들어갔는지 확인 : "+map.get("secondDate"));
 			
+		}else if(firstDate.equals("")&!secondDate.equals("")){
+			logger.debug("firstDate는 공백이고 secondDate는 공백이 아닐때");
+			map.put("firstDate", "1900-01-01");
+			map.put("secondDate", secondDate);
+			
+			logger.debug("firstDate 확인 : "+map.get("firstDate"));
+			logger.debug("secondDate 들어갔는지 확인 : "+map.get("secondDate"));
+			
+		}else if(!firstDate.equals("")&secondDate.equals("")){
+			logger.debug("firstDate는 값이 있고 secondDate가 공백일때");
+			map.put("firstDate", firstDate);
+			map.put("secondDate", currentdate);
+			
+			logger.debug("firstDate 확인 : "+map.get("firstDate"));
+			logger.debug("secondDate 들어갔는지 확인 : "+map.get("secondDate"));
+		}else{
+			logger.debug("firstDate, secondDate 모두 값이 있을때");
+			map.put("firstDate", firstDate);
+			map.put("secondDate", secondDate);
+			logger.debug("firstDate 확인 : "+map.get("firstDate"));
+			logger.debug("secondDate 들어갔는지 확인 : "+map.get("secondDate"));
 		}
+		
 		return map;
 	
 	}
