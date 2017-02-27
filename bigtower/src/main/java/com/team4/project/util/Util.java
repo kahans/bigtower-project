0354c0c5b6a212581ba20746a69aadbb41af2e8b
ryun215 <ryun215@naver.com>
2017-02-27 10:21:33 +0900

    Util.createDate() 주석추가

--- bigtower/src/main/java/com/team4/project/util/Util.java
+++ bigtower/src/main/java/com/team4/project/util/Util.java
@@ -10,16 +10,21 @@ import org.slf4j.LoggerFactory;
 
 public class Util {
 	private static final Logger logger = LoggerFactory.getLogger(Util.class);
-
+	//날짜검색시 공백인 칸에 날짜 만들어주는 메소드
 	public static Map<String, Object> createDate(String firstDate, String secondDate){
 		System.out.println("입력된 firstDate 확인 : "+firstDate);
 		System.out.println("입력된 secondDate 확인 : "+secondDate);
 		
 		Map<String, Object> map = new HashMap<String, Object>();
+		//Date객체를 생성하여 오늘날짜를 받아옴
 		Date date = new Date();
+		//Date의 형태를 yyyy-mm-dd로 바꿔줌
 		SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd");
+		//받아온 현재날짜를 yyyy-mm-dd형태로 바꿔줌
 		String currentdate = transFormat.format(date);
 		System.out.println("currentDate 확인 : "+currentdate);
+		
+		//if문을 통해 날짜검색시 입력된 날짜의 공백여부를 확인하여 값을 만들어 맵에 넣어줌
 		if(firstDate.equals("")&secondDate.equals("")){
 			logger.debug("둘다 공백일때");
 			map.put("firstDate", "1900-01-01");
@@ -49,7 +54,7 @@ public class Util {
 			logger.debug("firstDate 확인 : "+map.get("firstDate"));
 			logger.debug("secondDate 들어갔는지 확인 : "+map.get("secondDate"));
 		}
-		
+		//날짜값이 담긴 map을 리턴시킴
 		return map;
 	
 	}
