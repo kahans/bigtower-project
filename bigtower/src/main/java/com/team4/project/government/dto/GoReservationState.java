f05d9c471672bd62de35fa84b7cd04ef60445b67
sangil <gentlesangil@nate.com>
2017-03-06 11:11:21 +0900

    정부 패키지 삭제 및 프로젝트 수정

--- bigtower/src/main/java/com/team4/project/government/dto/GoReservationState.java
+++
@@ -1,5 +0,0 @@
-package com.team4.project.government.dto;
-
-public class GoReservationState {
-
-}
--- bigtower/src/main/java/com/team4/project/government/reservation/domain/GoReservation.java
+++
@@ -1,5 +0,0 @@
-package com.team4.project.government.reservation.domain;
-
-public class GoReservation {
-
-}
