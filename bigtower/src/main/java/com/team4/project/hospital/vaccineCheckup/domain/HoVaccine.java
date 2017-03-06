c6a14cb1ef4d8be03e149a58472dbaa8ae582260
Administrator <Administrator@smart20112>
2017-03-06 09:41:34 +0900

    백신DTO 작성

--- bigtower/src/main/java/com/team4/project/hospital/vaccineCheckup/domain/HoVaccine.java
+++ bigtower/src/main/java/com/team4/project/hospital/vaccineCheckup/domain/HoVaccine.java
@@ -2,4 +2,47 @@ package com.team4.project.hospital.vaccineCheckup.domain;
 
 public class HoVaccine {
 
+	protected String hoVaccineCode;
+	protected String hoTreatmentCode;
+	protected String hoVaccineTypeCode;
+	protected String hoVaccineDate;
+	protected int hoGoSendState;
+	public String getHoVaccineCode() {
+		return hoVaccineCode;
+	}
+	public void setHoVaccineCode(String hoVaccineCode) {
+		this.hoVaccineCode = hoVaccineCode;
+	}
+	public String getHoTreatmentCode() {
+		return hoTreatmentCode;
+	}
+	public void setHoTreatmentCode(String hoTreatmentCode) {
+		this.hoTreatmentCode = hoTreatmentCode;
+	}
+	public String getHoVaccineTypeCode() {
+		return hoVaccineTypeCode;
+	}
+	public void setHoVaccineTypeCode(String hoVaccineTypeCode) {
+		this.hoVaccineTypeCode = hoVaccineTypeCode;
+	}
+	public String getHoVaccineDate() {
+		return hoVaccineDate;
+	}
+	public void setHoVaccineDate(String hoVaccineDate) {
+		this.hoVaccineDate = hoVaccineDate;
+	}
+	public int getHoGoSendState() {
+		return hoGoSendState;
+	}
+	public void setHoGoSendState(int hoGoSendState) {
+		this.hoGoSendState = hoGoSendState;
+	}
+	@Override
+	public String toString() {
+		return "HoVaccine [hoVaccineCode=" + hoVaccineCode + ", hoTreatmentCode=" + hoTreatmentCode
+				+ ", hoVaccineTypeCode=" + hoVaccineTypeCode + ", hoVaccineDate=" + hoVaccineDate + ", hoGoSendState="
+				+ hoGoSendState + "]";
+	}
+	
+	
 }
