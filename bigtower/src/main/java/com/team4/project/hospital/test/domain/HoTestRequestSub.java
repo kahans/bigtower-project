1ca125eae976f817bc55700d1a3a37d1a21695df
Administrator <Administrator@smart20112>
2017-03-06 16:45:59 +0900

    HoTestRequestSub Dto에 병원코드 받아오도록 수정

--- bigtower/src/main/java/com/team4/project/hospital/test/domain/HoTestRequestSub.java
+++ bigtower/src/main/java/com/team4/project/hospital/test/domain/HoTestRequestSub.java
@@ -2,13 +2,22 @@ package com.team4.project.hospital.test.domain;
 
 public class HoTestRequestSub extends HoTestRequest{
 	protected String hoPatientName;
-
+	protected String HospitalCode;
 	public String getHoPatientName() {
 		return hoPatientName;
 	}
 
 	public void setHoPatientName(String hoPatientName) {
 		this.hoPatientName = hoPatientName;
+	}
+
+	
+	public String getHospitalCode() {
+		return HospitalCode;
+	}
+
+	public void setHospitalCode(String hospitalCode) {
+		HospitalCode = hospitalCode;
 	}
 
 	@Override
