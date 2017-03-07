f84189d341efa2d9ce006652b79873739166c938
loverman85 <loverman85@hanmail.net>
2017-03-07 09:42:04 +0900

    일단 커밋

--- bigtower/src/main/java/com/team4/project/hospital/transport/controller/TransportService.java
+++ bigtower/src/main/java/com/team4/project/hospital/transport/controller/TransportService.java
@@ -1,6 +1,8 @@
 package com.team4.project.hospital.transport.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.google.gson.Gson;
 import com.team4.project.hospital.diagnosisPrescription.domain.HoDiagnosis;
 import com.team4.project.hospital.diagnosisPrescription.domain.HoPrescription;
 import com.team4.project.hospital.hospitalizationOperation.domain.HoHospitalization;
@@ -25,6 +28,7 @@ public class TransportService {
 	private static final Logger logger = LoggerFactory.getLogger(TransportService.class);
 	@Autowired
 	private TransportDao transportDao;
+	private Gson gson = new Gson();
 	
 	/*
 	HoBloodTest
@@ -43,6 +47,8 @@ public class TransportService {
 		// http로 보낼때 db에 들어갈내용은 json String으로 변환
 		// Map<String, Object>타입으로 각 조회결과를 담는데 조회결과가 없는것은 null로 담는다.
 		// 파일은 파일이름 앞에 blood, checkup, image붙여서 보내서 받은다음에 잘라서 해당폴더에 넣는다
+		
+		Map<String, Object> map = new HashMap<String, Object>();
 		
 		// 혈액검사 결과 조회
 		List<HoBloodTest> hoBloodTest = transportDao.selectListHoBloodTest();
@@ -83,8 +89,6 @@ public class TransportService {
 		// 예방접종 조회
 		List<HoVaccine> hoVaccine = transportDao.selectListHoVaccine();
 		logger.debug("hoVaccine:"+hoVaccine);
-		
-		
 		
 	}
 }
