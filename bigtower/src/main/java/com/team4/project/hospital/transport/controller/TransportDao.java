6a8a82d5ad3f5470af5bbf43fcbeb16b53e19cb5
loverman85 <loverman85@hanmail.net>
2017-03-09 12:26:49 +0900

    정부전송여부 수정하는 dao, 매퍼 작성

--- bigtower/src/main/java/com/team4/project/hospital/transport/controller/TransportDao.java
+++ bigtower/src/main/java/com/team4/project/hospital/transport/controller/TransportDao.java
@@ -76,4 +76,57 @@ public class TransportDao {
 	public List<Map> selectListHoVaccine(String hospitalCode){
 		return sql.selectList("hoTransport.selectListHoVaccine", hospitalCode);
 	}
+	
+	
+	
+	// 정부전송상태 수정
+	// 차트 수정
+	public int updateChartSendState(){
+		return sql.update("hoTransport.updateChartSendState");
+	}
+	
+	// 진료 수정
+	public int updateTreatmentSendState(){
+		return sql.update("hoTransport.updateTreatmentSendState");
+	}
+	
+	// 진단 수정
+	public int updateDiagnosisSendState(){
+		return sql.update("hoTransport.updateDiagnosisSendState");
+	}
+	
+	// 처방 수정
+	public int updatePrescriptionSendState(){
+		return sql.update("hoTransport.updatePrescriptionSendState");
+	}
+	
+	// 입퇴원 수정
+	public int updateHospitalizationSendState(){
+		return sql.update("hoTransport.updateHospitalizationSendState");
+	}
+	
+	// 수술 수정
+	public int updateOperationSendState(){
+		return sql.update("hoTransport.updateOperationSendState");
+	}
+	
+	// 혈액검사 수정
+	public int updateBloodTestSendState(){
+		return sql.update("hoTransport.updateBloodTestSendState");
+	}
+	
+	// 영상검사 수정
+	public int updateMediaTestSendState(){
+		return sql.update("hoTransport.updateMediaTestSendState");
+	}
+	
+	// 건강검진 수정
+	public int updateCheckupSendState(){
+		return sql.update("hoTransport.updateCheckupSendState");
+	}
+	
+	// 예방접종 수정
+	public int updateVaccineSendState(){
+		return sql.update("hoTransport.updateVaccineSendState");
+	}
 }
