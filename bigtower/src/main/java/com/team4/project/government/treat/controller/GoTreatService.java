2ee03ef6e930482f40e8950f1300fbfe670f6a24
ryun215 <ryun215@naver.com>
2017-03-10 12:33:09 +0900

    treat citizenId로 조회해서 폼으로 받도록 수정해놓음 자리이동할거야!

--- bigtower/src/main/java/com/team4/project/government/treat/controller/GoTreatService.java
+++ bigtower/src/main/java/com/team4/project/government/treat/controller/GoTreatService.java
@@ -1,20 +1,26 @@
 package com.team4.project.government.treat.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.team4.project.government.diagnosisPrescription.domain.GoDiagnosis;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.team4.project.government.treat.domain.GoTreat;
-import com.team4.project.util.Util;
+import com.team4.project.util.ContextParam;
+import com.team4.project.util.HttpUrlCon;
 
 @Service
 public class GoTreatService {
 
+	Gson gson = new Gson();
 	@Autowired
 	private GoTreatDao goTCDao;
+	
 	
 	// 한사람의 하나의 진료정보
 	public GoTreat getOneTreat(String treatCode){
@@ -22,8 +28,29 @@ public class GoTreatService {
 	}
 		
 	// 한사람의 진료리스트
-	public List<GoTreat> getListTreatByCitizenId(String citizenId){
-		return goTCDao.selectListTreatByCitizenId(citizenId);
+	public List<GoTreat> getListTreatByCitizenId(String citizenId, String doctorId){
+		System.out.println("진료코드 조회 서비스 의사아이디 확인 : "+doctorId);
+		System.out.println("진료코드 조회 서비스 주민번호 확인 : "+citizenId);
+		
+		List<GoTreat> treatList = new ArrayList<GoTreat>();
+		String url = ContextParam.context.getInitParameter("receiveUrl");
+		HttpUrlCon conn = new HttpUrlCon(url+"/bigbang/government/getListTreatByCitizenId");
+		Map<String, String> map = new HashMap<String, String>(); 
+		map.put("doctorId", doctorId);
+		map.put("citizenId", citizenId);
+		try {
+			String list = conn.HttpUrlPOST(map);	//여기서 전송 해서 String으로 받아
+			
+			treatList = gson.fromJson(list, new TypeToken<List<GoTreat>>(){}.getType());
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return treatList;
 	}
 	
 	// 한명의 의사에게 진료받은 여러사람의 진료리스트
